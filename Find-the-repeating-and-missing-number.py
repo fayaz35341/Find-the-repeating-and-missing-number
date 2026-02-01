15aaa4eb9a1973302dbaa487ddfab671ff34a70a
Riyaz Shaik <94755295+fayaz35341@users.noreply.github.com>
2026-02-01 11:40:20 +0530

    Add function to find missing and repeated numbers
    Implement solution to find missing and repeated values in a 2D array.

--- Find-the-repeating-and-missing-number.py
+++ Find-the-repeating-and-missing-number.py
@@ -16,3 +16,26 @@ class Solution:
         x=int(val1+val2)//2
         y=int(x-val1)
         return [x,y]
+
+# //2965. Find Missing and Repeated Values
+class Solution:
+    def findMissingAndRepeatedValues(self, arr: List[List[int]]) -> List[int]:
+        N=len(arr)
+        n=N*N
+        # s1=sum(arr)
+        s1=0
+        s1n=int(n*(n+1)//2)
+        # s2=sum(i*i for i in arr)
+        s2=0
+        s2n=int(n*(n+1)*(2*n+1)//6)
+        for i in range(N):
+            for j in range(N):
+                s1+=arr[i][j]
+                s2+=arr[i][j]*arr[i][j]
+        val1=s1-s1n
+        val2=(s2-s2n)//val1
+        # val2=int(val2//val1)
+        x=int(val1+val2)//2
+        y=int(x-val1)
+        return [x,y] 
+        
